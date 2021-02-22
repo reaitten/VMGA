8f902fe17257ef42d685bd852cc9e8efc632e0ec
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-02-22 21:23:07 +0800

    Update linux-run.sh

--- linux-run.sh
+++ linux-run.sh
@@ -1,3 +1,5 @@
+#linux-run.sh LINUX_USER_PASSWORD
+
 sudo useradd -m koolisw
 sudo adduser koolisw sudo
 echo 'koolisw:1031' | sudo chpasswd
@@ -7,6 +9,10 @@ sudo apt -y install screen
 sudo apt -y install xfce4
 sudo apt -y install firefox
 sudo apt install tightvncserver
+umask 0077
+mkdir -p "$HOME/.vnc"
+chmod go-rwx "$HOME/.vnc"
+vncpasswd -f <<<"$1" >"$HOME/.vnc/passwd"
 tightvncserver :1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
