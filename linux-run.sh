ea542a5a8b26a49fecdcb57ad503b5a9f224b461
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-02-22 20:38:09 +0800

    Create linux-run.sh

---
+++ linux-run.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt -y install screen
+sudo apt -y install xfce4
+sudo apt -y install firefox
+sudo apt install tightvncserver
+tightvncserver :1
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
