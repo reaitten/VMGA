8644958367e07a16cab8303d63a404962f80f947
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-02-23 21:01:14 +0800

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -1,10 +1,10 @@
 #linux-run.sh LINUX_USER_PASSWORD NGROK_AUTH_TOKEN LINUX_USERNAME LINUX_MACHINE_NAME
 #!/bin/bash
 
-sudo apt-get update
-sudo useradd -m $LINUX_USERNAME
-sudo adduser $LINUX_USERNAME sudo
-echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
+# sudo apt-get update
+# sudo useradd -m $LINUX_USERNAME
+# sudo adduser $LINUX_USERNAME sudo
+# echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
 # sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo hostname $LINUX_MACHINE_NAME
 
