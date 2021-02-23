a2f6e01488a34ac3c4a06ec5c56d6cfeb95820da
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-02-23 08:09:43 +0800

    Update linux-run.sh

--- linux-run.sh
+++ linux-run.sh
@@ -5,7 +5,6 @@ sudo apt -y install screen
 sudo apt -y install xfce4
 sudo apt -y install firefox
 sudo apt install tightvncserver
-tightvncserver :1
-echo -e "$1\n$1" | sudo passwd
+tightvncserver :1 ; echo "$1\n$1" ; sudo passwd
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
