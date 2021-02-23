37ea4dd6b1ff5e023fb2a8872294599b89b7c1a7
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-02-23 21:04:34 +0800

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -1,11 +1,11 @@
 #linux-run.sh LINUX_USER_PASSWORD NGROK_AUTH_TOKEN LINUX_USERNAME LINUX_MACHINE_NAME
 #!/bin/bash
 
-# sudo apt-get update
-# sudo useradd -m $LINUX_USERNAME
-# sudo adduser $LINUX_USERNAME sudo
-# echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
-# sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+#sudo apt-get update
+sudo useradd -m $LINUX_USERNAME
+sudo adduser $LINUX_USERNAME sudo
+echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo hostname $LINUX_MACHINE_NAME
 
 if [[ -z "$NGROK_AUTH_TOKEN" ]]; then
