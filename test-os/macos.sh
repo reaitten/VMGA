b9cf99c443df9ea174f367e8392ec57483867206
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-02-24 08:30:11 +0800

    Create macos.sh

---
+++ test-os/macos.sh
@@ -0,0 +1,26 @@
+#macos-run.sh MAC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN MAC_REALNAME
+
+#disable spotlight indexing
+sudo mdutil -i off -a
+
+#Create new account
+sudo dscl . -create /Users/koolisw
+sudo dscl . -create /Users/koolisw UserShell /bin/bash
+sudo dscl . -create /Users/koolisw RealName $4
+sudo dscl . -create /Users/koolisw UniqueID 1001
+sudo dscl . -create /Users/koolisw PrimaryGroupID 80
+sudo dscl . -create /Users/koolisw NFSHomeDirectory /Users/koolisw
+sudo dscl . -passwd /Users/koolisw $1
+sudo dscl . -passwd /Users/koolisw $1
+sudo createhomedir -c -u koolisw > /dev/null
+
+#Enable VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+
+#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+
+#Start VNC/reset changes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
