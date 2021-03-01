ffe15c64f6f1f4e4f53f0a8ac7d83346c38ff821
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-03-01 18:51:07 +0800

    Update linux-desktop.sh

--- linux-desktop.sh
+++ linux-desktop.sh
@@ -8,6 +8,8 @@ sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt -y install screen
+sudo apt -y install xfce4
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
