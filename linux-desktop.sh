f234ca02784a153a099ac5d4e1dda172c3602ade
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-03-29 13:25:20 +0800

    Update linux-desktop.sh

--- linux-desktop.sh
+++ linux-desktop.sh
@@ -1,9 +1,12 @@
 #linux-run.sh LINUX_USER_PASSWORD NGROK_AUTH_TOKEN CHROME_HEADLESS_CODE LINUX_MACHINE_NAME LINUX_USERNAME GOOGLE_REMOTE_PIN
 #!/bin/bash
 
-sudo useradd -m $LINUX_USERNAME
-sudo adduser $LINUX_USERNAME sudo
-echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
+if [[ -z "$LINUX_USER_PASSWORD" ]]; then
+  echo "Please set 'LINUX_USER_PASSWORD' for user: $USER"
+  exit 3
+fi
+
+echo "runner:$LINUX_USER_PASSWORD" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -21,47 +24,10 @@ sudo apt install nautilus nano -y
 sudo apt install gdebi
 sudo apt -y install firefox
 sudo hostname $LINUX_MACHINE_NAME
-sudo adduser $LINUX_USERNAME chrome-remote-desktop
+sudo adduser runner chrome-remote-desktop
 sudo apt install tightvncserver
 echo -e "no\n$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | tightvncserver :1
-su - $LINUX_USERNAME -c """$CHROME_HEADLESS_CODE --pin=$GOOGLE_REMOTE_PIN"""
-
-if [[ -z "$NGROK_AUTH_TOKEN" ]]; then
-  echo "Please set 'NGROK_AUTH_TOKEN'"
-  exit 2
-fi
-
-if [[ -z "$LINUX_USER_PASSWORD" ]]; then
-  echo "Please set 'LINUX_USER_PASSWORD' for user: $USER"
-  exit 3
-fi
-
-echo "### Install ngrok ###"
-
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
+su - runner -c """$CHROME_HEADLESS_CODE --pin=$GOOGLE_REMOTE_PIN"""
 
 echo "### Update user: $USER password ###"
 echo -e "$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | sudo passwd "$USER"
-
-echo "### Start ngrok proxy for 22 port ###"
-
-
-rm -f .ngrok.log
-./ngrok authtoken "$NGROK_AUTH_TOKEN"
-./ngrok tcp 22 --log ".ngrok.log" &
-
-sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
-
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
-  echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "Connect with remote desktop: https://remotedesktop.google.com/access"
-  echo "=========================================="
-else
-  echo "$HAS_ERRORS"
-  exit 4
-fi
